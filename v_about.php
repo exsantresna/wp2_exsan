0e758552b3e9e7686dd1ee1a58f0e9814fc46e84
exsantresna <101534450+exsantresna@users.noreply.github.com>
2022-04-01 13:05:08 +0700

    tugas pertemuan 3

---
+++ v_about.php
@@ -0,0 +1,22 @@
+<section> 
+    <h1><?php echo $judul ?></h1>
+    <h4>Nama</h4> <ul type="disc">
+         <li>Nama Depan : exsan</li>
+         <li>Nama Belakang : tresna firdaus</li> 
+        </ul> 
+        <br> 
+        <h4>Alamat</h4> 
+        <ul type="disc"> 
+            <li> takokak - cianjur</li>
+         </ul>
+          <h4>Tempat Lahir</h4> 
+          <ul type="disc">
+               <li>cianjur</li> 
+            </ul>
+             <h4>Olah Raga Favorit</h4> 
+             <ul type="disc"> 
+                 <li>futsal</li>
+                  <li>bulutangkis</li>
+                 </ul> 
+                
+</section>
